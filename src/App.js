11bfd0377524617a4d4c7bf5bc40994640a1aa39
JoaoACastro <joaoaccastro@gmail.com>
2022-04-29 16:09:35 -0300

    main

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
-import Footer from './components/pages/Footer';
-import Navbar from './components/layout/Navbar';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
       <Navbar />
 
-      <Container>
+      <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="contact" element={<Contact />} />

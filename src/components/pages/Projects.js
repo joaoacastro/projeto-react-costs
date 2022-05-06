c06105a9507636f4a9aa8d00e573248c26c4b6af
JoaoACastro <joaoaccastro@gmail.com>
2022-05-06 12:16:21 -0300

    main

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+
+import styles from './Projects.module.css';
 
 function Projects() {
   const location = useLocation();
@@ -9,9 +12,14 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }

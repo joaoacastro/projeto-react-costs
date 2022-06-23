5f71368e6951a877160d9b90f46ff942d7ddb074
JoaoACastro <joaoaccastro@gmail.com>
2022-06-23 11:35:35 -0300

    main

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,7 +2,10 @@ import styles from "./Project.module.css";
 
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Projects from "./Projects";
+
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
+
 
 function Project() {
   const { id } = useParams();
@@ -10,20 +13,31 @@ function Project() {
   const [project, setProject] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((resp) => resp.json())
-      .then((data) => {
-        setProject(data);
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((err) => console.log);
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log);
+    }, 3000);
   }, [id]);
 
-  return <p>{project.name}</p>;
+  return <>{project.name ? 
+    <div>
+      <Container customClass="column">
+        <div>
+          <h1>Projeto: {project.name}</h1>
+          <button>Editar Projeto</button>
+        </div>
+      </Container>
+    </div>
+    : <Loading />}</>;
 }
 
 export default Project;
